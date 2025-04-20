6e024f26f76104052b5f8da1b7a0ee0f1166edc4
opti21 <40129778+opti21@users.noreply.github.com>
2025-04-19 19:47:46 -0500

    chore: comment out user and account schema definitions

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -6,7 +6,6 @@ import {
   sqliteTableCreator,
   text,
 } from "drizzle-orm/sqlite-core";
-import { type AdapterAccount } from "next-auth/adapters";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -40,68 +39,68 @@ export const requests = sqliteTable(
   }),
 );
 
-export const users = sqliteTable("user", {
-  id: text("id", { length: 255 }).notNull().primaryKey(),
-  name: text("name", { length: 255 }),
-  email: text("email", { length: 255 }).notNull(),
-  emailVerified: integer("emailVerified", {
-    mode: "timestamp",
-  }).default(sql`CURRENT_TIMESTAMP`),
-  image: text("image", { length: 255 }),
-});
+// export const users = sqliteTable("user", {
+//   id: text("id", { length: 255 }).notNull().primaryKey(),
+//   name: text("name", { length: 255 }),
+//   email: text("email", { length: 255 }).notNull(),
+//   emailVerified: integer("emailVerified", {
+//     mode: "timestamp",
+//   }).default(sql`CURRENT_TIMESTAMP`),
+//   image: text("image", { length: 255 }),
+// });
 
-export const usersRelations = relations(users, ({ many }) => ({
-  accounts: many(accounts),
-}));
+// export const usersRelations = relations(users, ({ many }) => ({
+//   accounts: many(accounts),
+// }));
 
-export const accounts = sqliteTable(
-  "account",
-  {
-    userId: text("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    type: text("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: text("provider", { length: 255 }).notNull(),
-    providerAccountId: text("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: text("token_type", { length: 255 }),
-    scope: text("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: text("session_state", { length: 255 }),
-  },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
-    }),
-    userIdIdx: index("account_userId_idx").on(account.userId),
-  }),
-);
+// export const accounts = sqliteTable(
+//   "account",
+//   {
+//     userId: text("userId", { length: 255 })
+//       .notNull()
+//       .references(() => users.id),
+//     type: text("type", { length: 255 })
+//       .$type<AdapterAccount["type"]>()
+//       .notNull(),
+//     provider: text("provider", { length: 255 }).notNull(),
+//     providerAccountId: text("providerAccountId", { length: 255 }).notNull(),
+//     refresh_token: text("refresh_token"),
+//     access_token: text("access_token"),
+//     expires_at: integer("expires_at"),
+//     token_type: text("token_type", { length: 255 }),
+//     scope: text("scope", { length: 255 }),
+//     id_token: text("id_token"),
+//     session_state: text("session_state", { length: 255 }),
+//   },
+//   (account) => ({
+//     compoundKey: primaryKey({
+//       columns: [account.provider, account.providerAccountId],
+//     }),
+//     userIdIdx: index("account_userId_idx").on(account.userId),
+//   }),
+// );
 
-export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
-}));
+// export const accountsRelations = relations(accounts, ({ one }) => ({
+//   user: one(users, { fields: [accounts.userId], references: [users.id] }),
+// }));
 
-export const sessions = sqliteTable(
-  "session",
-  {
-    sessionToken: text("sessionToken", { length: 255 }).notNull().primaryKey(),
-    userId: text("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    expires: integer("expires", { mode: "timestamp" }).notNull(),
-  },
-  (session) => ({
-    userIdIdx: index("session_userId_idx").on(session.userId),
-  }),
-);
+// export const sessions = sqliteTable(
+//   "session",
+//   {
+//     sessionToken: text("sessionToken", { length: 255 }).notNull().primaryKey(),
+//     userId: text("userId", { length: 255 })
+//       .notNull()
+//       .references(() => users.id),
+//     expires: integer("expires", { mode: "timestamp" }).notNull(),
+//   },
+//   (session) => ({
+//     userIdIdx: index("session_userId_idx").on(session.userId),
+//   }),
+// );
 
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
-}));
+// export const sessionsRelations = relations(sessions, ({ one }) => ({
+//   user: one(users, { fields: [sessions.userId], references: [users.id] }),
+// }));
 
 export const verificationTokens = sqliteTable(
   "verificationToken",
